7f9b57bbedae037295df64ca940b255effd568d9
Lokeshwaran421 <lokeshwarang421@gmail.com>
2022-05-27 12:00:23 +0530

    commit message

---
+++ src/test/java/test/git/GitTry.java
@@ -0,0 +1,15 @@
+package test.git;
+
+public class GitTry {
+	
+	private void m1() {
+System.out.println("m1");
+	}
+	private void m2() {
+System.out.println("m2");
+	}
+	private void m3() {
+System.out.println("m3");
+	}
+
+}
